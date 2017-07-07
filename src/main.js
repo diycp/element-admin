6f79bd07040be2b005c1c7143720fb12941c05f2
Vultur <wall-e@live.cn>
2017-07-07 15:48:03 +0800

    更新基本配置

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './app'
 import router from './router'
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+import 'font-awesome/css/font-awesome.min.css'
+import 'normalize.css'
+
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
@@ -9,5 +15,5 @@ new Vue({
   el: '#app',
   router,
   template: '<App/>',
-  components: { App }
+  components: {App}
 })
